f5ea7a6d9b83b63ab0008ff2933e3ae6d77688b5
Soumya-dash0708 <soumyadash945@gmail.com>
2023-06-29 17:28:46 +0530

    second comiit

---
+++ components/data.tsx
@@ -0,0 +1,60 @@
+export const About = [
+    {
+      key: 1,
+      image: "sun.png",
+      text1: "Low energy costs",
+  
+      text2:
+        "One of the most significant benefits of owning an Eco Haven Realty Home is lower energy costs. Our eco-friendly homes are designed to use less energy with our solar powered system, saving money on bills.",
+      
+      
+    },
+    {
+      key: 2,
+      image: "city.png",
+      text1: "Smaller carbon footprint ",
+      
+      text2:
+        "Our eco-friendly homes also have a smaller carbon footprint. They constantly use renewable energy sources and produce less waste which means they have a lower impact on the environment.",
+   
+      
+    },
+    {
+      key: 3,
+      image: "sky.png",
+      text1: "Indoor ",
+     
+      text2:
+        "Our hybrid ventilation strategy includes both natural and mechanical ventilation, while our airtight wooden structure reduces risk of mold development, ensuring a healthy home.",
+    
+     
+    },
+  ];
+  
+
+  export const Client = [
+    {
+      key: 1,
+      text1:
+        "““I had a wonderful experience working with EcoHaven Realty to find my new home. They really took the time to understand what was important to me and helped me find a home that was not only beautiful but also eco-friendly and energy-efficient. ”",
+      image: "q.png",
+      text2: "Felix Jimoh",
+      rating: 4,
+    },
+    {
+      key: 2,
+      text1:
+        "“Complete account of the system and expound the actual Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots”",
+      image: "q.png",
+      text2: "David T.",
+      rating: 5,
+    },
+    {
+      key: 3,
+      text1:
+        "“There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour”",
+      image: "q.png",
+      text2: "Clara R. Altman",
+      rating: 2,
+    },
+  ];
